c4d00c789483acc83d2b00007d33c4a1d07d24ce
Jose <sixgg4@gmail.com>
2019-04-08 20:26:44 -0400

    daily practice

---
+++ JavaScript/example.js
@@ -0,0 +1,15 @@
+//Numbers
+9
+9.3 
+
+// Strings
+"Hello World"
+
+// Booleans
+true 
+false
+
+// Null and undefined
+null 
+undefined
+
---
+++ JavaScript/stalker/script.js
@@ -0,0 +1,6 @@
+let fname = console.log("What is your first name? "))
+let lname = console.log(prompt("What is your last name? "))
+let age = console.log(prompt("What is your age"))
+
+console.log(`Your full name is ${fname} ${lname}`)
+console.log(`Oh and you are ${age} years old`)
