b8a6ffb8f1e8edb24d05040e4a67a87cc0dcbf40
Jose <sixgg4@gmail.com>
2019-05-01 20:23:47 -0400

    Methods practice

--- JavaScript/jQuery/advancedMethods/methods/main.js
+++ JavaScript/jQuery/advancedMethods/methods/main.js
@@ -4,3 +4,48 @@ $('h1:first').click(() => {
 $("button").click(function () {
     $(this).css("background", "pink")
 })
+
+// $("input").keypress(function () {
+//   console.log("You pressed a key")
+//   console.log($(this).val())
+// })
+$("input").keyup(function () {
+  console.log("You pressed a key")
+  console.log($(this).val())
+})
+$("input").on("keypress", function (e) {
+  console.log(e)
+  if (e.which === 13) {
+    alert("You hit enter")
+  }
+})
+let count = 1
+$('h1:first').next().on("click", (function (e) {
+  count++
+  if (count == 5) {
+    alert("Stop!")
+  }
+  console.log($(this).text())
+}));
+//Two ways of doing things 
+$("h1:eq(2)").on("click", function (e) {
+  $(this).css("color", "purple")
+})
+$("h1").on("click", function (e) {
+  $(this).css("color", "purple")
+})
+
+$("button").on("mouseenter", function (e) {
+  $(this).css("font-weight", "bold")
+})
+$("button").on("mouseleave", function (e) {
+  $(this).css("font-weight", "normal")
+})
+$("button").on("click", function(){
+  $('div').slideToggle(1000, function () {
+    console.log("completed")
+    $(this).remove();
+  });
+ });
+
+ 
