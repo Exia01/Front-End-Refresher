1bfbc9f141688eb03aab0b80f11725715636aea5
Jose <sixgg4@gmail.com>
2019-05-27 16:30:29 -0400

    set up new and index routes

--- blogApp/app.js
+++ blogApp/app.js
@@ -1,8 +1,10 @@
 const bodyParser = require('body-parser'),
   express = require('express'),
-  mongoose = require('mongoose');
+  mongoose = require('mongoose'),
+  path = require('path');
 
 app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 // App Congfig
 mongoose.connect(
   'mongodb+srv://admin:NdTest-1_1@main-cluster-hpysy.mongodb.net/test?retryWrites=true',
@@ -28,16 +30,52 @@ let blogSchema = new mongoose.Schema({
 
 let Blog = mongoose.model('Blog', blogSchema);
 
+
 //Restful Routes
 app.get('/', (req, res) => {
   res.redirect('/blogs');
 });
+// all blogs
 app.get('/blogs', (req, res) => {
   Blog.find({}, (err, blogs) => {
     if (err) {
       console.log('Failed to find blogs');
     } else {
-        res.render('index', {blogs:blogs});
+      res.render('index', { blogs: blogs });
+    }
+  });
+});
+
+app.get('/blogs/new', function(req, res) {
+  res.render('new');
+});
+
+
+// CREATE ROUTE
+app.post('/blogs', function(req, res) {
+  // create blog
+  console.log(req.body);
+  console.log('===========');
+  console.log(req.body);
+  Blog.create(req.body.blog, function(err, newBlog) {
+    if (err) {
+      res.render('new');
+    } else {
+      //then, redirect to the index
+      res.redirect('/blogs');
+    }
+  });
+});
+
+
+// SHOW ROUTE
+app.get('/blogs/:id', function(req, res) {
+  Blog.findById(req.params.id, (err, foundBlog) => {
+    if (err) {
+      console.log(err);
+      res.redirect('/blogs');
+    } else {
+      res.render('show', { blog: foundBlog });
     }
   });
 });
