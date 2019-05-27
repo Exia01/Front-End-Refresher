9afa8581f8f3e3d800cadd18ebe709267ae5a9a4
Jose <sixgg4@gmail.com>
2019-05-27 17:34:12 -0400

    completed update and show route

--- blogApp/app.js
+++ blogApp/app.js
@@ -1,7 +1,9 @@
 const bodyParser = require('body-parser'),
   express = require('express'),
   mongoose = require('mongoose'),
-  path = require('path');
+  path = require('path'),
+  expressSanitizer = require('express-sanitizer');
+  methodOverride = require('method-override')
 
 app = express();
 app.use(express.static(path.join(__dirname, 'public')));
@@ -15,8 +17,13 @@ mongoose.connect(
     reconnectInterval: 3000
   }
 );
+mongoose.set('useFindAndModify', false) //fixed random deprecation warning???
 
 app.set('view engine', 'ejs');
+//PUT method override
+app.use(methodOverride('_method'))
+// Mount express-sanitizer middleware here
+app.use(expressSanitizer());
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -30,12 +37,11 @@ let blogSchema = new mongoose.Schema({
 
 let Blog = mongoose.model('Blog', blogSchema);
 
-
 //Restful Routes
 app.get('/', (req, res) => {
   res.redirect('/blogs');
 });
-// all blogs
+// INDEX ROUTE
 app.get('/blogs', (req, res) => {
   Blog.find({}, (err, blogs) => {
     if (err) {
@@ -46,27 +52,27 @@ app.get('/blogs', (req, res) => {
   });
 });
 
+// NEW FORM
 app.get('/blogs/new', function(req, res) {
   res.render('new');
 });
 
-
 // CREATE ROUTE
 app.post('/blogs', function(req, res) {
   // create blog
-  console.log(req.body);
-  console.log('===========');
-  console.log(req.body);
-  Blog.create(req.body.blog, function(err, newBlog) {
+  // console.log(req.body);
+  // console.log('===========');
+  // console.log(req.body);
+  req.body.blog.body = req.sanitize(req.body.blog.body)
+  Blog.create(req.body.blog), function(err, newBlog) {
     if (err) {
       res.render('new');
     } else {
       //then, redirect to the index
       res.redirect('/blogs');
     }
-  });
+  };
 });
-
 
 // SHOW ROUTE
 app.get('/blogs/:id', function(req, res) {
@@ -78,6 +84,46 @@ app.get('/blogs/:id', function(req, res) {
       res.render('show', { blog: foundBlog });
     }
   });
+});
+
+// EDIT ROUTE
+app.get('/blogs/:id/edit', function(req, res) {
+  Blog.findById(req.params.id, function(err, foundBlog) {
+    if (err) {
+      console.log(err);
+      res.redirect('/blogs');
+    } else {
+      res.render('edit', { blog: foundBlog });
+    }
+  });
+});
+
+// UPDATE ROUTE
+app.put('/blogs/:id', function(req, res) {
+  req.body.blog.body = req.sanitize(req.body.blog.body);
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(
+    err,
+    updatedBlog
+  ) {
+    if (err) {
+      res.redirect('/blogs');
+    } else {
+      res.redirect(`/blogs/${req.params.id}`);
+    }
+  });
+});
+
+// DELETE ROUTE
+app.delete('/blogs/:id', function(req, res) {
+  //destroy blog
+  Blog.findByIdAndRemove(req.params.id, function(err) {
+    if (err) {
+      res.redirect('/blogs');
+    } else {
+      res.redirect('/blogs');
+    }
+  });
+  //redirect somewhere
 });
 
 let PORT = 8000;
