c5de32ad52e56d296825dae06a78e28367f70e12
exia01 <gonzalej@protonmail.com>
2019-06-21 15:53:23 -0400

    create app with local strategy

---
+++ NodeJS/passportJS/sampleApp/config/keys.js
@@ -0,0 +1,6 @@
+dbPassword = 'mongodb+srv://admin:WVm6fwYgsLyvOvHl@main-cluster-hpysy.mongodb.net/test?retryWrites=true&w=majority';
+// dbPassword = 'mongodb+srv://admin:'+ encodeURIComponent('WVm6fwYgsLyvOvHl') + '@main-cluster-hpysy.mongodb.ne/test?retryWrites=true';
+
+module.exports = {
+    mongoURI: dbPassword
+};
